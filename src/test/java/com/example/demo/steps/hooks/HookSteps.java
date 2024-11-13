540e73247b7947fa3d038ea2bc2bb0def3f29a2a
duongdt <duongdt>
2024-11-13 10:25:59 +0700

    Update log messages in HookSteps before and after all tests
    Changed the log messages for clarity in the hook steps. This ensures consistent terminology when marking the start and end of tests.

--- src/test/java/com/example/demo/steps/hooks/HookSteps.java
+++ src/test/java/com/example/demo/steps/hooks/HookSteps.java
@@ -6,12 +6,12 @@ import org.junit.jupiter.api.BeforeAll;
 public class HookSteps {
     @BeforeAll()
     public static void beforeAll() {
-        System.out.println("Before All");
+        System.out.println("before Test");
     }
 
     @AfterAll()
     public static void afterAll() {
-        System.out.println("After All");
+        System.out.println("After Test");
     }
 
 }
